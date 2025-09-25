356cae0d2cd137b2d5a82305b3d904b969590301
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-09-25 21:45:10 +0900

    x,y 동작 test

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -13,9 +13,11 @@ y = 90
 while running:
     clear_canvas()
     background.draw(400, 300)
-    boy.clip_draw(frame*100, 300, 100, 100, 400, 90)
+    boy.clip_draw(frame*100, 300, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
+    x+=5
+    y+=5
     delay(0.5)
 
 close_canvas()
