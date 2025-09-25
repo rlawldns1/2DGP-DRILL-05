6b43a124aedcf1200938bd36160f7987d0a5f64b
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-09-25 21:50:41 +0900

    SDL_QUIT로 종료조건구현 및 running global로 수정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,6 +6,11 @@ background = load_image('TUK_GROUND.png')
 boy = load_image('animation_sheet.png')
 
 def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
     pass
 
 running = True
