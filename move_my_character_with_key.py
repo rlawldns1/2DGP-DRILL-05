abfdbcf2a06d02ca581019138cd0707d6f0954d4
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-09-25 21:43:38 +0900

    frame 사용하여 애니메이션

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,12 +7,15 @@ boy = load_image('animation_sheet.png')
 
 running = True
 frame = 0
+x = 400
+y = 90
 
 while running:
     clear_canvas()
     background.draw(400, 300)
-    boy.clip_draw(frame*100, 100, 100, 100, 400, 90)
+    boy.clip_draw(frame*100, 300, 100, 100, 400, 90)
     update_canvas()
+    frame = (frame + 1) % 8
     delay(0.5)
 
 close_canvas()
