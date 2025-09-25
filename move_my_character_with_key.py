18b8139a73ba90a24673c54eb72f66719d53d65a
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-09-25 21:33:12 +0900

    boy 출력

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,10 +6,12 @@ background = load_image('TUK_GROUND.png')
 boy = load_image('animation_sheet.png')
 
 running = True
+frame = 0
 
 while running:
     clear_canvas()
     background.draw(400, 300)
+    boy.clip_draw(frame*100, 100, 100, 100, 400, 90)
     update_canvas()
     delay(0.5)
 
